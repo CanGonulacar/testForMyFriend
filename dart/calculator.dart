1e74b526f0fb8f7cdbb9886af91299f964755d1c
Can Gönülaçar <cgonulacar5@gmail.com>
2023-12-27 15:32:14 +0300

    Update calculator.dart

--- dart/calculator.dart
+++ dart/calculator.dart
@@ -1,7 +1,85 @@
 import 'dart:io';
-import 'dart:math';
 
 void main() {
+  while (true) {
+    print('Lütfen İşlem Giriniz');
+    String islem = stdin.readLineSync()!;
+
+    var veri = islem.split(RegExp(r'[+-/x]'));
+
+    if (islem.contains('x')) {
+      int first = int.parse(veri.first);
+      int second = int.parse(veri.last);
+      print('Sonuç: ${first * second}');
+    } else if (islem.contains('+')) {
+      int first = int.parse(veri.first);
+      int second = int.parse(veri.last);
+      print('Sonuç: ${first + second}');
+    } else if (islem.contains('-')) {
+      int first = int.parse(veri.first);
+      int second = int.parse(veri.last);
+      print('Sonuç: ${first - second}');
+    } else if (islem.contains('/')) {
+      int first = int.parse(veri.first);
+      int second = int.parse(veri.last);
+      print('Sonuç: ${first / second}');
+    }
+  }
+}
+
+sikiciMethodum(String data, List<int> first, second) {
+  if (data.contains('x') &&
+      data.contains('+') &&
+      data.contains('-') &&
+      data.contains('/')) {
+    if (data.contains('1')) {
+      second.add(1);
+    } else if (data.contains('2')) {
+      second.add(2);
+    } else if (data.contains('3')) {
+      second.add(3);
+    } else if (data.contains('4')) {
+      second.add(4);
+    } else if (data.contains('5')) {
+      second.add(5);
+    } else if (data.contains('6')) {
+      second.add(6);
+    } else if (data.contains('7')) {
+      second.add(7);
+    } else if (data.contains('8')) {
+      second.add(8);
+    } else if (data.contains('9')) {
+      second.add(9);
+    } else if (data.contains('0')) {
+      second.add(0);
+    }
+  } else {
+    if (data.contains('1')) {
+      first.add(1);
+    } else if (data.contains('2')) {
+      first.add(2);
+    } else if (data.contains('3')) {
+      first.add(3);
+    } else if (data.contains('4')) {
+      first.add(4);
+    } else if (data.contains('5')) {
+      first.add(5);
+    } else if (data.contains('6')) {
+      first.add(6);
+    } else if (data.contains('7')) {
+      first.add(7);
+    } else if (data.contains('8')) {
+      first.add(8);
+    } else if (data.contains('9')) {
+      first.add(9);
+    } else if (data.contains('0')) {
+      first.add(0);
+    }
+  }
+}
+
+
+/* void main() {
   while (true) {
     print('Toplama(1)');
     print('Cikarma(2)');
@@ -44,3 +122,4 @@ void main() {
     }
   }
 }
+ */
